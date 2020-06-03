4443073b8c2a0792401873220adcb7cdd5f51931
jguzz <joseph.a.guzzardo@gmail.com>
2020-06-03 17:17:29 -0400

    created collection

---
+++ db/migrate/20200603211624_create_collections.rb
@@ -0,0 +1,11 @@
+class CreateCollections < ActiveRecord::Migration[6.0]
+  def change
+    create_table :collections do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
