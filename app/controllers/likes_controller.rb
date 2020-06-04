d17980c71980da1408c49ce58a4b3d4ee72b99f6
jguzz <joseph.a.guzzardo@gmail.com>
2020-06-04 09:57:41 -0400

    index controllers created

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,16 @@
+class LikesController < ApplicationController
+	def create
+        @like= Like.new(like_params)
+        @like.save
+        render json:@like
+	end
+	def index
+        @likes = Likes.all
+        render json:@likes
+	end
+	
+	private
+    def like_params
+        params.require(:like).permit(:user_id, :post_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   resources :collections
   resources :users
+  resources :likes
+  resources :posts 
+  resources :stories 
+  resources :plants
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
